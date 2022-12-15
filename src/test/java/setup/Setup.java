122ae757b9f1167dac5d19c5f473546f322e7aeb
zadid-ahsan5274 <zadid.ahsan5274@gmail.com>
2022-12-15 15:02:21 +0600

    added gradle wrapper

--- src/test/java/setup/Setup.java
+++ src/test/java/setup/Setup.java
@@ -7,15 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class Setup {
 
     public WebDriver driver;
 
     @BeforeTest
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
